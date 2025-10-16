4b7f3f3fa2ce3e3626401510b18a322587e6bd04
Keks <keks@htmlacademy.ru>
2025-10-16 10:38:29 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/full-size.js
+++ 8/js/full-size.js
@@ -1,21 +1,14 @@
 import { isEscapeKey } from './util.js';
 import { userPhotos } from './data.js';
+import {clearComments, initComments} from './render-comments.js';
 
 const pictures = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
 const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
 const likesCount = bigPicture.querySelector('.likes-count');
-const socialCommentsLoader = bigPicture.querySelector('.social__comments-loader');
-const socialCommentShowCount = bigPicture.querySelector('.social__comment-shown-count');
-const socialCommentTotalCount = bigPicture.querySelector('.social__comment-total-count');
-const socialCommentList = bigPicture.querySelector('.social__comments');
-const socialCommentItem = socialCommentList.querySelector('.social__comment');
 const socialCaption = bigPicture.querySelector('.social__caption');
 
-let comments = [];
-let currentCommentsCount = 0;
-const COMMENTS_STEP = 5;
 
 // Обработчик клавиатуры
 const onDocumentKeyDown = (evt) => {
@@ -25,64 +18,6 @@ const onDocumentKeyDown = (evt) => {
   }
 };
 
-// Создание элемента комментария
-const createCommentElement = (comment) => {
-  const commentElement = socialCommentItem.cloneNode(true);
-  commentElement.querySelector('.social__picture').src = comment.avatar;
-  commentElement.querySelector('.social__picture').alt = comment.name;
-  commentElement.querySelector('.social__text').textContent = comment.message;
-  return commentElement;
-};
-
-// Создание блока комментариев
-const renderCommentsBlock = () => {
-  const commentElementFragment = document.createDocumentFragment();
-  const shownComments = comments.slice(currentCommentsCount, currentCommentsCount + COMMENTS_STEP);
-  const shownCommentsLength = shownComments.length + currentCommentsCount;
-
-  shownComments.forEach((comment) => {
-    const commentElement = createCommentElement(comment);
-    commentElementFragment.appendChild(commentElement);
-  });
-
-  socialCommentList.appendChild(commentElementFragment);
-
-  socialCommentShowCount.textContent = shownCommentsLength;
-  socialCommentTotalCount.textContent = comments.length;
-
-  if (shownCommentsLength >= comments.length) {
-    socialCommentsLoader.classList.add('hidden');
-  } else {
-    socialCommentsLoader.classList.remove('hidden');
-  }
-
-  currentCommentsCount += COMMENTS_STEP;
-};
-
-// Очистка комментариев
-const clearComments = () => {
-  currentCommentsCount = 0;
-  comments = [];
-  socialCommentList.innerHTML = '';
-  socialCommentsLoader.classList.add('hidden');
-  socialCommentsLoader.removeEventListener('click', renderCommentsBlock);
-};
-
-// Инициализация комментариев
-const initComments = (currentComments) => {
-  comments = currentComments;
-  currentCommentsCount = 0;
-  socialCommentList.innerHTML = '';
-
-  if (comments.length > COMMENTS_STEP) {
-    socialCommentsLoader.classList.remove('hidden');
-    socialCommentsLoader.addEventListener('click', renderCommentsBlock);
-  } else {
-    socialCommentsLoader.classList.add('hidden');
-  }
-
-  renderCommentsBlock();
-};
 
 // Функция заполнения данными
 const createFullPhotoData = (pictureId) => {
@@ -91,8 +26,6 @@ const createFullPhotoData = (pictureId) => {
   bigPictureImg.src = currentPhoto.url;
   bigPictureImg.alt = currentPhoto.description;
   likesCount.textContent = currentPhoto.likes;
-  socialCommentTotalCount.textContent = currentPhoto.comments.length;
-  socialCommentShowCount.textContent = currentPhoto.comments.length;
   socialCaption.textContent = currentPhoto.description;
 
   initComments(currentPhoto.comments);
