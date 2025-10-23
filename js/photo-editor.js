51d52f92e8f708460b515c9eb11cfbcd447fa865
Светлана Г <svetlgrish28@gmail.com>
2025-10-23 17:27:04 +0300

    Помощь друга

---
+++ js/photo-editor.js
@@ -0,0 +1,160 @@
+const uploadForm = document.querySelector('.img-upload__form');
+const imagePreview = uploadForm.querySelector('.img-upload__preview img');
+
+// Элементы масштабирования
+const scaleControlValue = uploadForm.querySelector('.scale__control--value');
+
+
+// Элементы эффектов
+const effectsList = uploadForm.querySelector('.effects__list');
+const effectLevelContainer = uploadForm.querySelector('.img-upload__effect-level');
+const effectLevelSlider = uploadForm.querySelector('.effect-level__slider');
+const effectLevelValue = uploadForm.querySelector('.effect-level__value');
+
+let currentScale = 100;
+
+const SCALE_STEP = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+
+// Эффекты
+const effectSettings = {
+  none: {
+    filter: 'none',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: ''
+  },
+  chrome: {
+    filter: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: ''
+  },
+  sepia: {
+    filter: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: ''
+  },
+  marvin: {
+    filter: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%'
+  },
+  phobos: {
+    filter: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px'
+  },
+  heat: {
+    filter: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: ''
+  }
+};
+
+const updateScale = (value) => {
+  currentScale = value;
+  scaleControlValue.value = `${value}%`;
+  imagePreview.style.transform = `scale(${value / 100})`;
+};
+
+
+const onScaleSmaller = () => {
+  if (currentScale > MIN_SCALE) {
+    currentScale -= SCALE_STEP;
+    updateScale(currentScale);
+  }
+};
+
+
+const onScaleBigger = () => {
+  if (currentScale < MAX_SCALE) {
+    currentScale += SCALE_STEP;
+    updateScale(currentScale);
+  }
+};
+
+
+const resetScaleEditor = () =>{
+  currentScale = 100;
+  updateScale(currentScale);
+};
+
+
+//Создаем слайдер
+noUiSlider.create(effectLevelSlider, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: (value) => {
+      const effect = document.querySelector('.effects__radio:checked').value || 'none';
+      return effectSettings[effect].step === 1 ? Math.round(value) : Number(value.toFixed(1));
+    },
+    from: (value) => parseFloat(value)
+  }
+
+});
+
+
+//Применяем эффект
+const applyEffect = (effect, value) => {
+  if (effect === 'none') {
+    imagePreview.style.filter = 'none';
+    effectLevelContainer.classList.add('hidden');
+    effectLevelValue.value = '';
+  } else {
+    const { filter, unit } = effectSettings[effect];
+    imagePreview.style.filter = `${filter}(${value}${unit})`;
+    effectLevelContainer.classList.remove('hidden');
+    effectLevelValue.value = value;
+  }
+};
+
+// Функция для сброса и обновления слайдера
+const resetSlider = (effect) => {
+  const {min, max, step} = effectSettings[effect];
+
+  effectLevelSlider.noUiSlider.updateOptions ({
+    range: {
+      min, max,
+    },
+    step,
+    start: max
+  });
+  applyEffect(effect, max);
+};
+
+
+effectsList.addEventListener('change', (evt)=> {
+  const radio = evt.target.closest('.effects__radio');
+  if (radio) {
+    const effect = radio.value;
+    resetSlider(effect);
+  }
+});
+
+
+effectLevelSlider.noUiSlider.on('update', () => {
+  const value = effectLevelSlider.noUiSlider.get();
+  const selectedEffect = document.querySelector('.effects__radio:checked').value;
+  applyEffect(selectedEffect, value);
+});
+
+
+export { onScaleSmaller, onScaleBigger, resetScaleEditor, resetSlider };
--- js/photo-form.js
+++ js/photo-form.js
@@ -1,5 +1,6 @@
 import { isEscapeKey } from './util.js';
-
+import { pristine } from './photo-form-validation.js';
+import { onScaleSmaller, onScaleBigger, resetScaleEditor, resetSlider } from './photo-editor.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const uploaderControll = uploadForm.querySelector('.img-upload__input');
@@ -9,10 +10,9 @@ const uploadCancel = uploadForm.querySelector('.img-upload__cancel');
 const hashtagInput = uploadForm.querySelector('.text__hashtags');
 const descriptionInput = uploadForm.querySelector('.text__description');
 
+const scaleSmaller = uploadForm.querySelector('.scale__control--smaller');
+const scaleBigger = uploadForm.querySelector('.scale__control--bigger');
 
-const HASHTAG_LENGTH = 20;
-const HASHTAG_ARRAY_LENGTH = 5;
-const COMMENT_LENGTH = 140;
 
 // Обработчик клавиатуры
 const onDocumentKeyDown = (evt) => {
@@ -39,6 +39,9 @@ const openImgUploader = () => {
   hashtagInput.addEventListener('keydown', onFieldKeyDown);
   descriptionInput.addEventListener('keydown', onFieldKeyDown);
 
+  scaleSmaller.addEventListener('click', onScaleSmaller);
+  scaleBigger.addEventListener('click', onScaleBigger);
+
 };
 
 
@@ -51,68 +54,6 @@ uploadCancel.addEventListener('click', (evt) => {
   evt.preventDefault();
   closeImgUploader();
 });
-
-
-const pristine = new Pristine(uploadForm, {
-  classTo: 'img-upload__field-wrapper',
-  errorClass: 'img-upload__field-wrapper--error',
-  errorTextParent: 'img-upload__field-wrapper',
-});
-
-
-// Валидация хэштегов
-const validateHashtags = (value) => {
-  if (value.trim() === '') {
-    return { valid: true };
-  }
-
-  const hashtagsArray = value.trim().split(' ').filter((tag) => tag.length > 0);
-  const seenHashtags = new Set();
-  const regex = /^#[a-zA-Zа-яА-ЯёЁ0-9]{1,19}$/;
-
-  if (hashtagsArray.length > HASHTAG_ARRAY_LENGTH) {
-    return { valid: false, message: `Нельзя указать больше ${HASHTAG_ARRAY_LENGTH} хэштегов` };
-  }
-
-  for (const hashtag of hashtagsArray) {
-    const lowerCaseHashtag = hashtag.toLowerCase();
-    if (!hashtag.startsWith('#')) {
-      return { valid: false, message: 'Хэштег должен начинаться с символа #' };
-    }
-    if (hashtag === '#') {
-      return { valid: false, message: 'Хэштег не может состоять только из решётки' };
-    }
-    if (hashtag.length > HASHTAG_LENGTH) {
-      return { valid: false, message: `Максимальная длина хэштега не более ${HASHTAG_LENGTH} символов` };
-    }
-    if (hashtag.indexOf('#', 1) !== -1) {
-      return { valid: false, message: 'Хэштеги должны разделяться пробелами' };
-    }
-    if (!regex.test(hashtag)) {
-      return { valid: false, message: 'Хэштег содержит недопустимые символы' };
-    }
-    if (seenHashtags.has(lowerCaseHashtag)) {
-      return { valid: false, message: 'Хэштеги не должны повторяться' };
-    }
-    seenHashtags.add(lowerCaseHashtag);
-  }
-
-  return { valid: true };
-};
-
-const validateResult = (value) => validateHashtags(value).valid;
-
-const validateErrorMessage = (value) => validateHashtags(value).message;
-
-//Валидация хэштегов
-pristine.addValidator(hashtagInput, validateResult, validateErrorMessage);
-
-
-//Валидация формы комментария
-const validateDescriptionInput = (value) => value.length <= COMMENT_LENGTH;
-
-pristine.addValidator(descriptionInput, validateDescriptionInput, `Не более ${COMMENT_LENGTH} символов`);
-
 
 uploadForm.addEventListener('submit', (evt) => {
   const isValid = pristine.validate();
@@ -130,6 +71,11 @@ function closeImgUploader () {
   hashtagInput.removeEventListener('keydown', onFieldKeyDown);
   descriptionInput.removeEventListener('keydown', onFieldKeyDown);
 
+  scaleSmaller.removeEventListener('click', onScaleSmaller);
+  scaleBigger.removeEventListener('click', onScaleBigger);
+
+  resetSlider('none');
+  resetScaleEditor();
 
   uploadForm.reset();
   pristine.reset();
