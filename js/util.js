3b75062a6e3b9fcaa5e14e88a67333358e1c7df5
Светлана Г <svetlgrish28@gmail.com>
2025-11-05 17:42:49 +0300

    удаление лишних функций

--- js/util.js
+++ js/util.js
@@ -1,37 +1,3 @@
-const MAX_PHOTO_ID = 25;
-const MAX_COMMENT_ID = 1000;
-const MAX_PHOTO_NUMBER = 25;
-
-
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-// Генератор ID и уникальных номеров
-function createRandomIdGenerator(min, max) {
-  const previousValues = [];
-
-  return function() {
-    if (previousValues.length >= (max - min + 1)) {
-      previousValues.length = 0; // Очищаем массив
-    }
-    let currentValue = getRandomInteger(min, max);
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-}
-
-const generatePhotoId = createRandomIdGenerator(1, MAX_PHOTO_ID);
-const generateCommentId = createRandomIdGenerator(1, MAX_COMMENT_ID);
-const generatePhotoUrl = createRandomIdGenerator(1, MAX_PHOTO_NUMBER);
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
@@ -46,6 +12,6 @@ function debounce (callback, timeoutDelay = 500) {
 }
 
 
-export {getRandomInteger, getRandomArrayElement, generatePhotoId, generateCommentId, generatePhotoUrl, isEscapeKey, debounce };
+export { isEscapeKey, debounce };
 
 
